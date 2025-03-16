f0aac5f17f529cbdb0d22f4469d5bc267af2bce5
noriyotcp <noriyo.akita@gmail.com>
2025-03-17 00:23:35 +0900

    Install eslint and config

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,4 +4,9 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   extensionApi: 'chrome',
   modules: ['@wxt-dev/module-react'],
+  imports: {
+    eslintrc: {
+      enabled: 9,
+    },
+  },
 });
