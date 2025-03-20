38ed46a9b08a9b04371cb827675a14f46f553dd5
noriyotcp <noriyo.akita@gmail.com>
2025-03-20 22:36:56 +0900

    Implement Theme Controller
    Switch theme by Theme Controller which is the feature of daisyUI
    
    toggle light and dracula theme
    light is default but it prefers to dracula when the system uses dark
    mode.

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -5,8 +5,45 @@ interface ThemeSwitcherProps {
   onThemeToggle: () => void;
 }
 
-const ThemeSwitcher = ({ theme, onThemeToggle }: ThemeSwitcherProps) => (
-  <button onClick={onThemeToggle}>{theme === 'light' ? 'Dark Mode' : 'Light Mode'}</button>
-);
+const ThemeSwitcher = ({ theme, onThemeToggle }: ThemeSwitcherProps) => {
+  return (
+    <label className="flex cursor-pointer gap-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <circle cx="12" cy="12" r="5" />
+        <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+      </svg>
+      <input
+        type="checkbox"
+        value="dracula"
+        className="toggle theme-controller"
+        onChange={onThemeToggle}
+        checked={theme === 'dark'}
+      />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />
+      </svg>
+    </label>
+  );
+};
 
 export default ThemeSwitcher;
