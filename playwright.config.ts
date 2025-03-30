f3ad0b5937ab1de7bc06536c803e94cdee54ce51
noriyotcp <noriyo.akita@gmail.com>
2025-03-30 10:36:27 +0900

    Cleanup playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,13 +5,8 @@ export default defineConfig({
   testDir: 'e2e',
   fullyParallel: true,
   reporter: 'html',
-  globalSetup: fileURLToPath(new URL('./global-setup', import.meta.url)), // globalSetup を指定
+  globalSetup: fileURLToPath(new URL('./global-setup', import.meta.url)),
   projects: [
-    // projects を追加
-    // { // extension-setup は削除
-    //   name: 'extension-setup', // setup project を定義
-    //   testMatch: /global\.setup\.ts/, // global.setup.ts を testMatch に指定
-    // },
     {
       name: 'chromium',
       use: {
@@ -21,9 +16,8 @@ export default defineConfig({
         launchOptions: {
           args: [],
         },
-        storageState: 'storageState.json', // storageState を use に追加
+        storageState: 'storageState.json',
       },
-      // dependencies: ['extension-setup'], // dependencies は削除
     },
   ],
 });
