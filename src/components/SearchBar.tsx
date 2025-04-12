3d7f98a3842bf739413af98a9ca3891d54a20d55
noriyotcp <noriyo.akita@gmail.com>
2025-04-12 13:58:25 +0900

    feat: Enhance SearchBar component with label and icon, and add keyboard shortcut indicator

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -6,15 +6,24 @@ interface SearchBarProps {
 }
 
 const SearchBar = forwardRef<HTMLInputElement, SearchBarProps>(({ searchQuery, onSearchQueryChange }, ref) => (
-  <input
-    id="search-bar"
-    type="search"
-    placeholder="Search tabs..."
-    value={searchQuery}
-    onChange={e => onSearchQueryChange(e.target.value)}
-    className="p-2.5 border border-gray-300 text-base w-full lg:basis-[50%]"
-    ref={ref}
-  />
+  <label className="input w-full lg:basis-[50%] rounded-none">
+    <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2.5" fill="none" stroke="currentColor">
+        <circle cx="11" cy="11" r="8"></circle>
+        <path d="m21 21-4.3-4.3"></path>
+      </g>
+    </svg>
+    <input
+      id="search-bar"
+      type="search"
+      placeholder="Search tabs..."
+      value={searchQuery}
+      onChange={e => onSearchQueryChange(e.target.value)}
+      className="grow p-2.5 text-base"
+      ref={ref}
+    />
+    <kbd className="kbd kbd-sm rounded-none bg-transparent">/</kbd>
+  </label>
 ));
 
 SearchBar.displayName = 'SearchBar'; // Add display name for better debugging
